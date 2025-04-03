71c8c739c344db0799905e5d8415b650e5fdd83d
Vinh Le <vinh.le@thesofinet.com>
2025-04-03 15:27:30 +0700

    Implement Wordle game

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -4,6 +4,7 @@ import com.wordle.WordGuesser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -11,25 +12,46 @@ public class Main {
 
     public static void main(String[] args) {
         LOGGER.info("Welcome to the Wordle game!");
-        LOGGER.info("Enter the length of word to start: ");
-
+        LOGGER.info("Choose options to start the game: ");
+        LOGGER.info("1. Guess word with the length");
+        LOGGER.info("2. Guess random word");
         Scanner scanner = new Scanner(System.in);
-
+        int option = 0;
         int length = 0;
-        boolean isLengthValid = false;
-        while (!isLengthValid) {
-            length = scanner.nextInt();
-            if (length < 0) {
-                LOGGER.info("Invalid length: The length of the word must be greater than 0");
+        boolean isValidOption = false;
+        while (!isValidOption) {
+            option = scanner.nextInt();
+
+            if (option == 1) {
+                LOGGER.info("Enter the length of word to start: ");
+                boolean isLengthValid = false;
+                while (!isLengthValid) {
+                    length = scanner.nextInt();
+                    if (length < 0 || length > 22) {
+                        LOGGER.info("Invalid length: The length of the word must be in range 1-22");
+
+                    } else {
+                        isLengthValid = true;
+                    }
+                }
+                isValidOption = true;
+
+            } else if (option == 2) {
+                Random random = new Random();
+                length = random.nextInt(22) + 1;
+
+                LOGGER.info("Guess word with length: {}", length);
+                isValidOption = true;
 
             } else {
-                isLengthValid = true;
+                LOGGER.info("Invalid option: Please choose 1 or 2!");
             }
         }
 
         scanner.close();
 
         try {
+            LOGGER.info("Start guessing the word.");
             String guessWord = WordGuesser.guess(length);
             LOGGER.info("Guessed word: {}", guessWord);
 
